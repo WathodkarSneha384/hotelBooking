a46a67ada0140a87f7f29f92fda93a93e7beb78b
dev2401 <dev2401@tnsofts.com>
2025-03-21 14:33:44 +0530

    added changed

--- src/components/HeroComponent.jsx/HeroComponent.jsx
+++ src/components/HeroComponent.jsx/HeroComponent.jsx
@@ -6,10 +6,10 @@ function HeroComponent() {
     <div className="hero-content">
         <p>ENJOY YOUR WONDERFUL HOLIDAYS WITH A GREAT LUXURY EXPERIENCE!</p>
         <h1>Most Relaxing Place</h1>
-        <div className="buttons">
+        {/* <div className="buttons">
           <button className="tour-btn">Take a Tour</button>
           <button className="learn-btn">Learn More</button>
-        </div>
+        </div> */}
       </div>
   )
 }
