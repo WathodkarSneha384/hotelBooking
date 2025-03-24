4dd0363d36bbb3a7830a0f97dcd6093f76a0b411
Vaibhavi <kshirsagarvaibhavi28@gmail.com>
2025-03-24 14:59:15 +0530

    made active links in footer and changed title name and icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,15 +25,15 @@ const Footer=()=>{
                         {" "}
                         <FaFacebook className="text-blue-500"/>
                         {" "}
-                        <a href="" className="hover:underline text-gray-300">Facebook</a>
+                        <a href="https://www.facebook.com/" target="_blank" className="hover:underline text-gray-300">Facebook</a>
                     </li>
                     <li>
                         <FaTwitter className="text-sky-500"/>
-                        <a href="" className="hover:underline text-gray-300">Twitter</a>
+                        <a href="https://x.com/" target="_blank" className="hover:underline text-gray-300">Twitter</a>
                     </li>
                     <li>
                         <FaInstagram className="text-orange-500"/>
-                        <a href="" className="hover:underline text-gray-300">Instagram</a>
+                        <a href="https://www.instagram.com/" target="_blank" className="hover:underline text-gray-300">Instagram</a>
                     </li>
                 </ul>
             </div> 
@@ -48,13 +48,13 @@ const Footer=()=>{
                 <ul>
                   <li className="ptmt-4 pt-6=6">
                     <FaEnvelope className="text-blue-500"/>
-                    <a href="" className="hover:underline text-gray-300">Unwind@gmail.com</a>
+                    <a href="mailto:unwind@gmail.com" className="hover:underline text-gray-300">Unwind@gmail.com</a>
                   </li>
                   <li className="mt-4 pt-6=6">
-                    <FaMobileAlt className="text-blue-500"/><a href="" className="hover:underline text-gray-300">8787656545</a>
+                    <FaMobileAlt className="text-blue-500"/><a href="tel:7058637155" className="hover:underline text-gray-300">8787656545</a>
                   </li>
                   <li className="pt=mt-4 pt-6">
-                    <FaAddressCard className="text-blue-500"/><a href="" className="hover:underline text-gray-300">KUNAL PLAZA, off Mumbai Pune Highway, Chinchwad, Pimpri-Chinchwad, Maharashtra 411019</a>
+                    <FaAddressCard className="text-blue-500"/><a href="https://maps.app.goo.gl/4B1ubfrfcbsbfD828" target="_blank" className="hover:underline text-gray-300">KUNAL PLAZA, off Mumbai Pune Highway, Chinchwad, Pimpri-Chinchwad, Maharashtra 411019</a>
                   </li>
 
                 </ul>
@@ -70,27 +70,6 @@ const Footer=()=>{
                     <li><a href="#" className="hover:underline text-gray-300">About</a></li>
                 </ul>
             </div>
-            {/* <div>
-                <h2 className="text-lg font-bold mb-4 text-white">
-                     Follow Us
-                </h2>
-                <ul className="flex space-x-4">
-                    <li>
-                        {" "}
-                        <FaFacebook className="text-blue-500"/>
-                        {" "}
-                        <a href="" className="hover:underline text-gray-300">Facebook</a>
-                    </li>
-                    <li>
-                        <FaTwitter className="text-sky-500"/>
-                        <a href="" className="hover:underline text-gray-300">Twitter</a>
-                    </li>
-                    <li>
-                        <FaInstagram className="text-orange-500"/>
-                        <a href="" className="hover:underline text-gray-300">Instagram</a>
-                    </li>
-                </ul>
-            </div> */}
         </div>
         <div className="border-t border-gray-600 pt-6 text-gray-300 text-center mt-6">
             <p>© 2025 Code With UnWind Hotels Booking. All Rights Reserved.</p>
