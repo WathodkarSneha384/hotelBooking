d2a2f8eb9a057214124fc4096b3c10b419278c1b
Vaibhavi <kshirsagarvaibhavi28@gmail.com>
2025-03-25 19:16:04 +0530

    check

--- src/components/HeroComponent.jsx/Pages/HomePages.jsx
+++ src/components/HeroComponent.jsx/Pages/HomePages.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Home from '../../Home'
 import BookingForm from '../BookingForm'
-//import HeroComponent from '../HeroComponent'
 import Footer from '../../Footer'
 
 
 function HomePages() {
   return (
-    <div>
-    <Home/>
+   <div>
+   <Home/>
    <BookingForm/>
    <Footer/>
    </div>
