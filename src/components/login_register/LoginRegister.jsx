5b250ed1bafe0ae11c4ae4e6d77b6a83f746ba65
Vaibhavi <kshirsagarvaibhavi28@gmail.com>
2025-04-09 14:04:25 +0530

    added back btn in login

--- src/components/login_register/LoginRegister.jsx
+++ src/components/login_register/LoginRegister.jsx
@@ -7,6 +7,9 @@ export default function LoginRegister() {
     const [isExistingUser,setIsExistingUser]=useState(true)
   return (
     <div>
+      <div className='d-flex'>
+      <a href='/' className='btn btn-warning ms-auto p-2 m-2'>Back</a>
+      </div>
 
     {isExistingUser? <SignIn newUser={setIsExistingUser}/>:<SignUp existingUser={setIsExistingUser}/>}
      
